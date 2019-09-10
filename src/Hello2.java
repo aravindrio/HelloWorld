bd3565b7ea6ffa7098272a7d176dc6990fa72c73
aravindrio <aravind.gtr@gmail.com>
2019-09-09 20:01:44 -0700

    Variable Program

---
+++ src/Hello2.java
@@ -0,0 +1,18 @@
+public class Hello2 {
+
+    public static void main(String[] args){
+        //We are going to deal with Variables
+        System.out.println("Hello, World!");
+        //In variables, it should define a datatype, name it and assign a value
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myFourthNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber +mySecondNumber + myThirdNumber;
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myFourthNumber);
+
+    }
+}
